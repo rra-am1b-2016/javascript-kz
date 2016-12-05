f70edb3358c680d9f8e78d3bfe4fe7fe886542ff
Arjan de Ruijter <rra@mboutrecht.nl>
2016-12-05 12:47:25 +0100

    Json

---
+++ js_json.js
@@ -0,0 +1,6 @@
+//alert("Dit is javascript in een extern bestand");
+// Hieronder staat een JSON (Javascript Object Notation) object
+
+var credentials = '{"firstname": "Arjan", "infix": "de", "lastname": "Ruijter" }';
+
+var jsObject = JSON.parse(credentials);
--- js_objects.js
+++ js_objects.js
@@ -83,14 +83,14 @@ var school = function(name, address, schooltype, educationTime){
    this.schooltype = schooltype;
    this.educationTime = educationTime;
    this.showInfo = function(){
-      var output =  "Ik zit op: " + school1.name + "<br>";
-          output += "Het adres is: " + school1.address + "<br>";
-          output += "Het schooltye is: " + school1.schooltype + "<br>";
+      var output =  "Ik zit op: " + this.name + "<br>";
+          output += "Het adres is: " + this.address + "<br>";
+          output += "Het schooltype is: " + this.schooltype + "<br>";
       return output;
    }
 }
 
-school1 = new school("MBO Utrecht", "Columbuslaan 540", "MBO", 4);
+var school1 = new school("MBO Utrecht", "Columbuslaan 540", "MBO", 4);
 
 school.prototype.date = function()
 {
@@ -99,6 +99,77 @@ school.prototype.date = function()
 
 document.write("Ik zit op: " + school1.showInfo() + "<br>");
 document.write("De datum is: " + school1.date());
+
+
+var rapport = function(name, webdev, gamedev, ned)
+{
+    if ( ned === undefined)
+    {
+        this.ned = "Nog geen cijfer toegevoegd";
+    }
+    else
+    {
+        this.ned = ned;
+    }
+    this.name = name;
+    this.webdev = webdev;
+    this.gamedev = gamedev;
+    this.showProps = function()
+    {
+        var output = "<br>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<br>";
+            output += "Mijn naam is: " + this.name + "<br>";
+            output += "Mijn cijfer voor webdevelopment is: " + this.webdev + "<br>";
+            output += "Mijn cijfer voor gamedevelopment is: " + this.gamedev + "<br>";
+            output += "Mijn cijfer voor Nederlands is: " + this.ned + "<br>";
+            output += "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<br>";
+        return output;            
+    }   
+} 
+
+var rapport1 = new rapport("Arjan de Ruijter", 3.5, 8.1, 2.1);
+var rapport2 = new rapport("", 9.8, 2.2, 2.3);
+document.write(rapport1.showProps());
+document.write(rapport2.showProps());
+
+// Maak een prototype met een verzameling gegeven die je zelf interessant vindt.
+// Minimaal 3 argumenten voor het prototype.
+// Maak een prototype van de nummer 1 t/m 3 van de Formule 1 over de jaren 2010 t/m 2016
+
+formuleI = function(year, first, second, third, constructorsTitle)
+{
+    this.year = year;
+    this.first = first;
+    this.second = second;
+    this.third = third;
+    this.constructorsTitle = constructorsTitle;
+    this.showData = function(){
+        var output = "<br>-----------------------<br>";
+            output += "uitslag WK: " + this.year + "<br>";
+            output += "-----------------------<br>";            
+            output += "Nummer 1: " + this.first + "<br>";
+            output += "Nummer 2: " + this.second + "<br>";
+            output += "Nummer 3: " + this.third + "<br>";
+            output += "Constructeurs titel: " + this.constructorsTitle + "<br>";
+            output += "-----------------------<br>";
+            return output;
+    }
+}
+
+var Y_2010 = new formuleI("2010", "Vettel", "Alonso", "Webber", "Ferrari");
+var Y_2011 = new formuleI("2011", "Vettel", "Button", "Webber", "Ferrari");
+var Y_2012 = new formuleI("2012", "Vettel", "Alonso", "Raikkonen", "Ferrari");
+var Y_2013 = new formuleI("2013", "Vettel", "Alonso", "Webber", "Ferrari");
+var Y_2014 = new formuleI("2014", "Hamilton", "Rosberg", "Ricciardo", "Mercedes");
+var Y_2015 = new formuleI("2015", "Hamilton", "Rosberg", "Vettel", "Mercedes");
+var Y_2016 = new formuleI("2016", "Rosberg", "Hamilton", "Vettel", "Mercedes");
+document.write(Y_2010.showData());
+document.write(Y_2011.showData());
+document.write(Y_2012.showData());
+document.write(Y_2013.showData());
+document.write(Y_2014.showData());
+document.write(Y_2015.showData());
+document.write(Y_2016.showData());
+
 
 
 
