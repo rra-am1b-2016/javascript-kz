997d0b17d7a4f9a3d861a607d7b74a46fd3aa911
Arjan de Ruijter <rra@mboutrecht.nl>
2016-12-05 14:50:36 +0100

    JSON Array met javascript objecten

--- js_json.js
+++ js_json.js
@@ -1,6 +1,22 @@
 //alert("Dit is javascript in een extern bestand");
-// Hieronder staat een JSON (Javascript Object Notation) object
+// Hieronder staat een JSON (Javascript Object Notation) string \
 
-var credentials = '{"firstname": "Arjan", "infix": "de", "lastname": "Ruijter" }';
+var credentials = '{"firstname": "Arjan",' +
+                   '"infix": "de",' +
+                   '"lastname": "Ruijter", ' +
+                   '"haarkleur": "zwart", ' +
+                   '"schoenmaat": 10.5, ' + 
+                   '"favoColors": ["yellow", "green", "red"], ' +    // Ook array's zijn mogelijk
+                   '"auto": [{"merk": "Ford", "Type": "mondeo", "Kenteken": "51-HB-ZP"}, ' + 
+                            '{"merk": "Daf", "Type": "variomatic", "Kenteken": "52-IC-AQ"}], ' +  //ook objecten
+                   '"married": true }';  // en booleans
+
+console.log(credentials);
+
+// Maak een JSON string die haarkleur en schoenmaat bevat en geef deze weer op js_json.html
+//var personelInfo = '{"haarkleur": "zwart", "schoenmaat": 10.5}';
 
 var jsObject = JSON.parse(credentials);
+
+//var jsPersonelInfoObject = JSON.parse(personelInfo);
+jsObject.test();
