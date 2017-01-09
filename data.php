0d1246d2f5b001c210e26847bed64971df147d18
Arjan de Ruijter <rra@mboutrecht.nl>
2017-01-09 12:48:33 +0100

    Ajax

--- data.php
+++ data.php
@@ -1,6 +1,25 @@
 <?php
-   //$outputArray = array("firstname" => "Arjan");
-   //echo json_encode($outputArray);
-   $output = '{"firstname": "Arjan", "infix": "de"}';
-   echo $output;
+   if (isset($_GET["id"]))
+   {
+      $id = $_GET["id"];
+   }
+   else
+   {
+      $id = 1;
+   }
+
+   $server_name = "	mysql.hostinger.nl";
+   $user_name = "u533841462_rra";
+   $password = "Ezekkthoes!1";
+   $db_name = "u533841462_rra";
+
+   $conn = mysqli_connect($server_name, $user_name, $password, $db_name);
+
+   $query = "SELECT * FROM `users` WHERE `id` = ".$id;
+
+   $result = mysqli_query($conn, $query);
+
+   $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+   echo json_encode($record);  
 ?>
--- js_httprequest.js
+++ js_httprequest.js
@@ -6,15 +6,27 @@ xmlHttp.onreadystatechange = function(){
       var jsObj = JSON.parse(xmlHttp.responseText);
       //console.debug(jsObj);
       //alert(jsObj.firstname);
-      document.getElementsByTagName("p")[0].innerHTML = jsObj.firstname;
-
+      document.getElementsByTagName("p")[0].innerHTML = jsObj.firstname + " " +
+                                                        jsObj.infix + " " +
+                                                        jsObj.lastname;
    }
 }
 
-xmlHttp.open("GET", "http://localhost/2016-2017/am1b/javascript-kz/data.php", true);
+
 
 document.getElementsByTagName("button")[0].onclick = function(){
+   xmlHttp.open("GET", "http://localhost/2016-2017/am1b/javascript-kz/data.php?id=6", true);
    xmlHttp.send();
+}
+
+var divs = document.getElementsByTagName("div");
+
+for ( var i = 0; i < divs.length; i++)
+{
+   divs[i].onmouseenter = function(){
+      xmlHttp.open("GET", "http://localhost/2016-2017/am1b/javascript-kz/data.php?id=" + this.innerHTML, true);
+      xmlHttp.send();
+   }
 }
 
 
